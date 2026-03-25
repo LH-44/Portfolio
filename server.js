26016c3c2a6392898cf762953ba742d883abbb2c
Lalit Kiran <lalit.kiran10@gmail.com>
2026-03-25 22:32:52 +0530

    fixed railway binding issue

--- server.js
+++ server.js
@@ -48,6 +48,6 @@ app.get("/admin/feedback", async (req, res) => {
 // ✅ Start server (ALWAYS KEEP THIS AT BOTTOM)
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log("Server running on port", PORT);
 });
